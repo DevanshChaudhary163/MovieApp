7342ab6121f72fde3dd4e61922884f62c4d4639f
Devansh Chaudhary <chaudharydevansh163@gmail.com>
2025-12-28 21:09:26 +0530

    A letterboxd inspired project with a simple favorites section to keep track

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "2ed1a9444e2b074358ea6c20b7459b93";
+const BASE_URL = "https://api.themoviedb.org/3";
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
